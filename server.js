ff813fa971be10c8172cf62c53589b4f522df3a0
Brian <brianzzl@hotmail.com>
2020-03-10 01:09:16 -0400

    trying to deploy with heroku, mongo_atlas uri

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/db");
 const app = express();
 
 connectDB();
+
 //Init Middleware
 app.use(express.json({ extended: false }));
 
@@ -13,6 +14,16 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/todo", require("./routes/api/todo"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
